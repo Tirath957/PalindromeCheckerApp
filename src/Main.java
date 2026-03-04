5d08228078e1ecce8b9488e925d8bcd5f3e84903
Tirath957 <ktirath001@gmail.com>
2026-03-05 01:47:40 +0530

    UC3: Palindrome check using string reverse

---
+++ src/Main.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+import java.util.Stack;
+
+public class PalindromeCheckerApp {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter a string:");
+        String input = scanner.nextLine();
+
+        Stack<Character> stack = new Stack<>();
+
+        // Push characters into stack
+        for(int i = 0; i < input.length(); i++) {
+            stack.push(input.charAt(i));
+        }
+
+        String reversed = "";
+
+        // Pop characters from stack
+        while(!stack.isEmpty()) {
+            reversed = reversed + stack.pop();
+        }
+
+        // Compare original and reversed
+        if(input.equals(reversed)) {
+            System.out.println("It is a Palindrome");
+        } else {
+            System.out.println("Not a Palindrome");
+        }
+    }
+}
